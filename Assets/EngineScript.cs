a9bb6b954bba26d00ed8586b1c8f79839a740e8a
kekos91 <kekos91@free.fr>
2017-12-26 20:47:55 +0100

    Initial commit

---
+++ Assets/EngineScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineScript : MonoBehaviour {
+    public GameObject[] hoverPoints;
+    public float desiredHeight = 0.2f;
+    public float hoverForce = 3f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();    
+    }
+    
+    private void FixedUpdate()
+    {
+        foreach(GameObject hoverPoint in hoverPoints)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(hoverPoint.transform.position, -hoverPoint.transform.up, out hit, desiredHeight )) {
+                float forceToApply = hoverForce * (1f - (hit.distance / desiredHeight));
+                rb.AddForceAtPosition(Vector3.up * forceToApply, hoverPoint.transform.position);
+            }
+        }
+    }
+}
